c881113492b0bfdcd63e318de0768ddb4012462b
Mengsen-W <mengsen_wang@163.com>
2020-03-10 19:39:32 +0800

    HelloSocket

--- HelloSocket/Source/main.cpp
+++ HelloSocket/Source/main.cpp
@@ -1,1 +1,21 @@
-int main(void) { return 0; }
+// 防止头文件的宏定义矛盾
+#define WIN32_LEAN_AND_MEAN
+
+#include <WinSock2.h>
+#include <windows.h>
+
+// 添加链接库
+#pragma comment(lib, "ws2_32.lib")
+
+int main(void) {
+  WORD ver = MAKEWORD(2, 2);
+  WSADATA data;
+  int back;
+
+  // 启动 Socket
+  back = WSAStartup(ver, &data);
+
+  // 停止 Socket
+  WSACleanup();
+  return 0;
+}
